1ef3c5a4f1f40c2a3e4f40d2b792c3e3a1b03977
luarrufat <larrufat@pioix.edu.ar>
2025-08-27 11:11:36 -0300

    avances en el registro

--- front/src/app/login/page.js
+++ front/src/app/login/page.js
@@ -5,19 +5,31 @@ import Input from "@/componentes/Input"
 import Title from "@/componentes/Title"
 import Mensajes from "@/componentes/Mensajes"
 import Link from 'next/link';
+import { useEffect, useState } from "react"
 export default function loginPage() {
     const [nombre, setNombre] = useState("");
     const [contraseña, setContraseña] = useState("");
+    const [usuarioMail, setUsuario] = useState("");
     function obtenerDatosRegistro(event) {
         setContraseña(event.target.value);
         setNombre(event.target.value);
+        
 
         let datos = {
             nombre: nombre,
-            password: contraseña
+            contraseña: contraseña,
+            usuario_mail: usuarioMail
         }
         agregarUsuarioRegistro(datos)
     }
+
+    /*
+    useEffect(
+        ()=>{
+            console.log(usuarioMail)
+        }, [usuarioMail]
+    )
+     */
 
     async function agregarUsuarioRegistro(datos) {
         try {
@@ -44,14 +56,18 @@ export default function loginPage() {
         <>  
             <Title texto="Registro"></Title>
             <h3>Ingresa tus datos</h3>
-            <Input color={"registro"} type={"text"} placeholder={"Ingrese su nombre"} id={"usuario_mail"}></Input>
             <br></br>
-            <Boton1 type={"text"} texto={"Enviar"} color={"wpp"} > Enviar</Boton1>
+            {/*Puse en el onChange una funcion de una sola linea para cambiar el usuario cuando cambie el valor del input*/}
+            <Input color={"registro"} type={"text"} placeholder={"Ingrese su mail"} id={"usuario_mail"} onChange={(event) =>setUsuario( event.target.value)}></Input>
             <br></br>
             <br></br>
-            <Input color={"registro"} type={"password"} placeholder={"Ingrese su contraseña"} id={"usuario_mail"}></Input>
+            <Input color={"registro"} type={"password"} placeholder={"Ingrese su contraseña"} id={"contraseña"}></Input>
             <br></br>
-            <Boton1 type={"text"} texto={"Enviar"} color={"wpp"}></Boton1>
+            <br></br>
+            <Input color={"registro"} type={"text"} placeholder={"Ingrese su nombre"} id={"nombre"}></Input>
+            <br></br>
+            <br></br>
+            <Boton1 type={"text"} texto={"Enviar"} color={"wpp"} onClick={obtenerDatosRegistro}>Enviar</Boton1>
         </>
     )
 } 
