7196afb5080616c14b003b47c3897a896a84bd0a
EmiliaGaetani <egaetani@pioix.edu.ar>
2025-08-28 10:10:48 -0300

    avanzando con la interfaz de inicio
    (agregar componente contacto para acceder desde un boton)

--- front/src/app/inicio/page.js
+++ front/src/app/inicio/page.js
@@ -2,18 +2,55 @@
 
 import Input from "@/componentes/Input"
 import Title from "@/componentes/Title"
-import Usuario from "@/componentes/Usuario"
 
-export default function inicioPage(){
+export default function inicioPage() {
+    let contacts = [
+        {
+            nombre: "Emi",
+            apellido: "Gaetani",
+            usuario_mail: "egaetani@gmail",
+            password: "sarmiento01",
+            foto_perfil: "",
+            descripción: "hola",
+        },
+        {
+            nombre: "Lu",
+            apellido: "Arrufat",
+            usuario_mail: "larrufat@gmail",
+            password: "juani",
+            foto_perfil: "",
+            descripción: "chau",
+        },
+        {
+            nombre: "Ju",
+            apellido: "Zuran",
+            usuario_mail: "jzuran@gmail",
+            password: "juju123",
+            foto_perfil: "",
+            descripción: "adios",
 
-    return(
+        },
+    ]
+
+      function mostrarContactos() {
+        let lista = []
+        for (let i = 0; i < contacts.length; i++) {
+            lista.push(
+                <li>
+                    {contacts[i].nombre} {contacts[i].apellido}
+                </li>
+            )
+        }
+        return lista
+    }
+
+    return (
         <>
-        <Title texto="Chats"></Title>
-        <Input type={text} placeholder={"Buscar"} id="buscar"></Input>
-        <ol>
-            <li><Usuario></Usuario> </li>
-        </ol>
+            <Title texto="Chats" />
+            <Input type="text" placeholder="Buscar" id="buscar" />
+            <ol>
+                {mostrarContactos()}
+            </ol>
         </>
     )
-
 }
