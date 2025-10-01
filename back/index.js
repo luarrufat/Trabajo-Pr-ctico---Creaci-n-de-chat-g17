4f657ae898632150994dae837b4fc66a0ba817dd
EmiliaGaetani <egaetani@pioix.edu.ar>
2025-10-01 08:43:06 -0300

    intento de que no traiga chat si el nombre de un usuario es vacio

--- back/index.js
+++ back/index.js
@@ -158,7 +158,8 @@ app.post("/chats", async function (req, res) {
            FROM Chats
            INNER JOIN UsuariosPorChat ON UsuariosPorChat.id_chat = Chats.ID
            WHERE UsuariosPorChat.id_usuario = "${req.body.id_usuario}"
-           AND (Chats.es_grupo = 0 OR (Chats.es_grupo = 1 AND Chats.nombre IS NOT NULL AND Chats.nombre != ''))
+           AND (Chats.es_grupo = 1 AND Chats.nombre IS NOT NULL AND Chats.nombre != "")
+           AND (Chats.es_grupo = 0 AND Chats.nombre IS NOT NULL AND Chats.nombre != "")
        `);
         res.send(resultado);
     } catch (error) {
